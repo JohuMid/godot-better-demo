807685aa12e60eb0379549011f757aef28abced5
JohuMid <JohuMid@outlook.com>
2026-02-10 20:33:24 +0800

    feat(物品): 添加箱子物品并调整障碍物碰撞检测
    - 新增箱子物品资源及配置
    - 修改Box.gd脚本添加箱子到组
    - 调整障碍物碰撞检测逻辑以支持箱子交互
    - 更新Level1和Level2场景配置

--- scripts/components/Barricade.gd
+++ scripts/components/Barricade.gd
@@ -8,6 +8,9 @@ var down_distance: float
 var detector: Area2D
 var ground_offset: float = 24
 
+var tween_down: Tween
+var tween_up: Tween
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# 初始化玩家射线查询参数（只创建一次）
@@ -27,9 +30,13 @@ func _ready() -> void:
 	
 
 func _on_player_entered(body: Node2D) -> void:
-	if body is CharacterBody2D:
+	if body and body.is_in_group("player"):
+		print("Player hit!",body)
 		var facing = body._check_facing_dir()
 		body.take_hit(Vector2(150 * -facing, 0))
+	if body and body.is_in_group("box"):
+		print("Box hit!",body)
+		_end_attack()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -51,7 +58,7 @@ func _process(delta: float) -> void:
 	player_ray.to = global_position + Vector2(0, 200)
 
 	var result = get_world_2d().direct_space_state.intersect_ray(player_ray)
-	
+
 	if result and result.collider.is_in_group("player"):
 		is_attacking = true
 		_start_attack()
@@ -60,21 +67,25 @@ func _process(delta: float) -> void:
 func _start_attack():
 	# 向下动画（0.5秒下落）
 	var target_y = original_position.y + down_distance
-	var tween_down = create_tween()
+	tween_down = create_tween()
 	tween_down.tween_property(self, "global_position:y", target_y, 0.3)
 	await tween_down.finished
 
+	_end_attack()
 	# 播放攻击动画
 	$AnimationPlayer.play("attack")
 	# 等待 0.5 秒（攻击持续时间）
 	await get_tree().create_timer(0.5).timeout
+
+
+	# 结束攻击状态
+	is_attacking = false
+
+func _end_attack():
 	# 回到原位（0.5秒上升）
-	var tween_up = create_tween()
+	tween_up = create_tween()
 	tween_up.tween_property(self, "global_position", original_position, 0.3)
 	await tween_up.finished
 
 	# 播放攻击动画
 	$AnimationPlayer.play_backwards("attack")
-
-	# 结束攻击状态
-	is_attacking = false
--- scripts/components/Box.gd
+++ scripts/components/Box.gd
@@ -2,4 +2,5 @@ extends RigidBody2D
 
 # Box.gd
 func _ready():
+	add_to_group("box")
 	pass
